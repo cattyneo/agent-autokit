86915cf2ce60d241976776aa00e7f10263d00e6b
cattyneo <cattyneo@gmail.com>
2026-05-04 18:55:33 +0900

    AK-005 core config runtime schema
    Closes #6

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,25 @@
 export const CORE_PACKAGE = "@cattyneo/autokit-core";
 
+export type {
+  AutokitConfig,
+  PhaseConfig,
+  PromptContractId,
+  Provider,
+  RuntimePhase,
+} from "./config.js";
+export {
+  ConfigParseError,
+  DEFAULT_CONFIG,
+  parseConfig,
+  parseConfigYaml,
+  phasePromptContracts,
+  runtimePhases,
+} from "./config.js";
 export type { ChildEnv, ParentEnv, RunnerEnvOptions } from "./env-allowlist.js";
 export { buildGhEnv, buildRunnerEnv } from "./env-allowlist.js";
+export type { ModelResolver, ResolvedModels, ResolveModelsOptions } from "./model-resolver.js";
+export {
+  createEmptyResolvedModels,
+  ModelResolutionError,
+  resolveModelsForPlanning,
+} from "./model-resolver.js";
